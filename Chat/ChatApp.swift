31b4a79c98fb33f5ad42e3fbb4b0fca5c9fb6b3a
Sean King <sking@ingeniaservices.com>
2021-02-19 18:19:06 -0500

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Sean King on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
