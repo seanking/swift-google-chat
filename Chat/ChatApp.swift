5d85cd9f3bb3866cd3fee963f9c25c3d7bea5a41
Sean King <sking@ingeniaservices.com>
2021-02-20 12:58:49 -0500

    Initial Commit of Chats

--- Chat/ChatApp.swift
+++ Chat/ChatApp.swift
@@ -5,13 +5,25 @@
 //  Created by Sean King on 2/19/21.
 //
 
+import AppKit
 import SwiftUI
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        
+        NSApplication.shared.windows.forEach { window in
+            window.setFrameAutosaveName("Main Window")
+            window.isReleasedWhenClosed = false
+        }
+    }
+}
 
 @main
 struct ChatApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WebBrowserView(url: URL(string: "https://chat.google.com")!)
         }
     }
 }
--- Chat/ContentView.swift
+++ Chat/ContentView.swift
@@ -6,16 +6,51 @@
 //
 
 import SwiftUI
+import WebKit
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct WebBrowserView : NSViewRepresentable {
+    
+    let url: URL
+    
+    public func makeNSView(context: Context) -> WKWebView {
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.applicationNameForUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Safari/605.1.15"
+        
+        let view = WKWebView(frame: .zero, configuration: webConfiguration)
+        view.navigationDelegate = context.coordinator
+        view.uiDelegate = context.coordinator
+        view.autoresizingMask = [.width, .height]
+        
+        return view
+        
     }
+    
+    public func updateNSView(_ nsView: WKWebView, context: Context) {
+        nsView.load(URLRequest(url: url))
+    }
+    
+    class Coordinator : NSObject, WKNavigationDelegate, WKUIDelegate {
+        func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+            if let requestURL = navigationAction.request.url {
+                print(requestURL.absoluteString)
+                
+                if let openURL = URL(string: requestURL.absoluteString) {
+                    NSWorkspace.shared.open(openURL)
+                }
+                
+            }
+            return nil
+        }
+    }
+    
+    func makeCoordinator() -> WebBrowserView.Coordinator {
+        Coordinator()
+    }
+    
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct WebBrowserViewDemo: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        WebBrowserView(url: URL(string: "https://chat.google.com")!)
     }
 }
