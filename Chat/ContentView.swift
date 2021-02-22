1c34f9d75c12e87cffcfd8c70d2ab538528646bf
Sean King <sking@ingeniaservices.com>
2021-02-22 17:08:26 -0500

    Support the Upload of Files

--- Chat/ContentView.swift
+++ Chat/ContentView.swift
@@ -53,6 +53,20 @@ struct WebBrowserView : NSViewRepresentable {
             }
             return nil
         }
+        
+        func webView(_ webView: WKWebView, runOpenPanelWith parameters: WKOpenPanelParameters, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping ([URL]?) -> Void) {
+            let openPanel = NSOpenPanel()
+            openPanel.canChooseFiles = true
+            openPanel.begin { result in
+                if result == NSApplication.ModalResponse.OK {
+                    if let url = openPanel.url {
+                        completionHandler([url])
+                    }
+                } else if result == NSApplication.ModalResponse.cancel {
+                    completionHandler(nil)
+                }
+            }
+        }
     }
     
     func makeCoordinator() -> WebBrowserView.Coordinator {
