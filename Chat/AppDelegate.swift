4375ed14e322be749899b0b944c410e697e530eb
Sean King <r.sean.king@gmail.com>
2021-03-13 14:31:52 -0500

    Extracted Methods to Improve Readability

--- Chat/AppDelegate.swift
+++ Chat/AppDelegate.swift
@@ -7,20 +7,7 @@ import UserNotifications
 class AppDelegate: NSObject, NSApplicationDelegate {
     var window: NSWindow!
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        let contentView = WebView()
-
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.isReleasedWhenClosed = false
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
-        
-        
+    fileprivate func requestAuthorizationForNotifications() {
         let notificationCenter = UNUserNotificationCenter.current()
         notificationCenter.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
             let logger = Logger()
@@ -29,8 +16,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             if let error = error {
                 logger.error("Error occurred requesting notification center authorization.")
                 logger.error("\(error.localizedDescription)")
-            }            
+            }
         }
+    }
+    
+    fileprivate func createWindow() -> NSWindow {
+        let window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            backing: .buffered, defer: false)
+        window.isReleasedWhenClosed = false
+        window.center()
+        window.setFrameAutosaveName("Main Window")
+        window.contentView = NSHostingView(rootView: WebView())
+        window.makeKeyAndOrderFront(nil)
+        
+        return window
+    }
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        window = createWindow()
+        requestAuthorizationForNotifications()
     }
     
     func applicationDidBecomeActive(_ notification: Notification) {
